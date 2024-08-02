e919edf89968a2a220d8dfa074c4ef582260fbc7
noname759 <umarovamirjon832@gmail.com>
2024-08-02 20:46:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkupgi
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 
 TOKEN = '7461614480:AAEQXlEnZAa_GK3ZypKZV_JFmJd-NOUwSJo'
 
 def start(update: Update, context: CallbackContext) -> None:
-    message = 'Привет я человек'
+    with open('photo.jpg', 'rb') as photo_file:
+        update.message.reply_photo(photo_file, caption='Привет я человек')
+
     button1 = InlineKeyboardButton("talk", callback_data='1')
     button2 = InlineKeyboardButton("contact", callback_data='2')
     button3 = InlineKeyboardButton("voice", callback_data='3')
-    button4 = InlineKeyboardButton("group", callback_data='4')
 
-    keyboard = [[button1, button2], [button3, button4]]
+    keyboard = [[button1, button2], [button3]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
     update.message.reply_text('Выберите кнопку:', reply_markup=reply_markup)
 
 def voice(update: Update, context: CallbackContext) -> None:
-    with open('.jpg', 'rb') as photo_file, open('.ogg', 'rb') as voice_file:
-        update.message.reply_photo(photo_file, caption=message)
+    with open('hello.ogg', 'rb') as voice_file:
         update.message.reply_voice(voice_file, caption="Голосовое сообщение")
 
 def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     choice = query.data
     query.answer()
-    query.edit_message_text(text=f"Вы выбрали кнопку {choice}")
 
-def talk(update: Update, context: CallbackContext) -> None:
+    if choice == '1':
+        talk(query, context)  
+    elif choice == '2':
+        contact(query, context) 
+    elif choice == '3':
+        query.edit_message_text(text="Вы выбрали кнопку voice")
+        query.message.reply_voice(open('hello.ogg', 'rb'), caption="Голосовое сообщение")
+
+def talk(query, context: CallbackContext) -> None:
     message = 'Мой аккаунт @Saw_Cuten'
-    update.message.reply_text(message)
+    query.message.reply_text(message) 
 
-def contact(update: Update, context: CallbackContext) -> None:
+def contact(query, context: CallbackContext) -> None:
     message = '+992 935844412 \n Нету второго номера :('
-    update.message.reply_text(message)
+    query.message.reply_text(message) 
 
 def main() -> None:
-    updater = Updater(TOKEN)
+    updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler('start', start))
@@ -47,5 +54,5 @@ def main() -> None:
     updater.start_polling()
     updater.idle()
 
-if name == 'main':
+if __name__ == '__main__':
     main()
